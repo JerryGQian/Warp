b661fdabc53c2853bf5661bb154e384dbedb9474
Gary Qian <gqian1@jhu.edu>
2017-09-16 08:36:55 -0400

    Make launchable app

--- LauncherDemo/app/src/main/java/com/example/andrei/customlauncherdemo/ConfigActivity.java
+++ LauncherDemo/app/src/main/java/com/example/andrei/customlauncherdemo/ConfigActivity.java
@@ -1,13 +1,19 @@
 package com.example.andrei.customlauncherdemo;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import backend.*;
 
-public class ConfigActivity extends AppCompatActivity {
+public class ConfigActivity extends Activity {
+
+    Settings settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config3);
+
+        settings = new Settings(getSharedPreferences("WarpSettings", 0));
     }
 }
